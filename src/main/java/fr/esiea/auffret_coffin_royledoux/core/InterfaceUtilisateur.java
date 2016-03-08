83311b60e8bd85457caabf5efaa0712599af8ea5
Floffly <f.royledoux@gmail.com>
2016-03-09 00:40:11 +0100

    entrées clavier

--- src/main/java/fr/esiea/auffret_coffin_royledoux/core/InterfaceUtilisateur.java
+++ src/main/java/fr/esiea/auffret_coffin_royledoux/core/InterfaceUtilisateur.java
@@ -3,9 +3,38 @@ package fr.esiea.auffret_coffin_royledoux.core;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class InterfaceUtilisateur implements KeyListener {
+import fr.esiea.auffret_coffin_royledoux.generationPiece.Piece;
+import fr.esiea.auffret_coffin_royledoux.mouvement.Rotation;
 
+
+public class InterfaceUtilisateur implements KeyListener {
+	
+	Piece p;
+	Rotation r;
+	
 	public void keyTyped(KeyEvent e) {
+		
+		switch(e.getKeyCode()) {
+		
+			case KeyEvent.VK_LEFT: 
+				if (p != null)
+				 r.antiHoraire(p);
+				
+			case KeyEvent.VK_RIGHT:
+				if (p != null)
+				r.horaire(p);
+			//default:
+						
+		}
+		
+	}
+
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/main/java/fr/esiea/auffret_coffin_royledoux/mouvement/Rotation.java
+++ src/main/java/fr/esiea/auffret_coffin_royledoux/mouvement/Rotation.java
@@ -1,6 +1,15 @@
 package fr.esiea.auffret_coffin_royledoux.mouvement;
 
+import fr.esiea.auffret_coffin_royledoux.generationPiece.Piece;
+
+
 public class Rotation {
 
+	public void horaire(Piece p) {
+			
+	}
 	
+	public void antiHoraire(Piece p) {
+		
+	}
 }
